91646a15880101b726124f6ebc19f4dd6726666f
nelsonic <contact.nelsonic+github@gmail.com>
2017-05-12 08:36:42 +0100

    fix start script (duh!) npm server.js > node server.js :confused:

--- server.js
+++ server.js
@@ -4,8 +4,8 @@ var path = require('path');
 
 http.createServer(function handler (req, res) { // can you make it simpler? ;-)
   console.log(req.method, ':', req.url); // rudimentary request logging
-  return fs.readFile(path.resolve('./index.html'), function (err, data) {
+  fs.readFile(path.resolve('./index.html'), function (err, data) {
     res.writeHead(200, {'Content-Type': 'text/html'});
     res.end(data);
   });
-}).listen(process.env.PORT); // start the server with the command: npm run dev
+}).listen(process.env.PORT || 8000); // start the server with the command: npm run dev
